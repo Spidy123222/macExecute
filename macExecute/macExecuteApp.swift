d68681062cd7b9c2b4ee3682a5c25594ec83eb26
Stossy11 <69031796+stossy11@users.noreply.github.com>
2025-04-22 20:08:09 +1000

    Initial Commit

---
+++ macExecute/macExecuteApp.swift
@@ -0,0 +1,17 @@
+//
+//  macExecuteApp.swift
+//  macExecute
+//
+//  Created by Stossy11 on 22/04/2025.
+//
+
+import SwiftUI
+
+@main
+struct macExecuteApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
